83202132b0b3226f769af7928df3b316b185b95d
Dibesh Panta <pantadibesh@gmail.com>
2026-08-09 18:34:13 +0545

    Delete pong-plus/pong directory

--- pong-plus/pong/src/PlayerPaddle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Paddle.h"
-
-// Human-controlled paddle. Supports both W/S and Up/Down arrow keys so it
-// works regardless of keyboard layout, plus optional mouse Y control.
-class PlayerPaddle : public Paddle {
-public:
-    void Update(float dt, float screenHeight);
-};
